45bd687107d1c8ba62d4412798f8d738d07c8b3d
galaxygcodergirl <101486713+galaxygcodergirl@users.noreply.github.com>
2022-12-19 20:12:18 +1100

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,26 @@
+img = "";
+status = "";
+function preload(){
+    img = loadImage("fruit.jpg");
+}
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+}
